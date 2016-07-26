d6ea8b6cb23b3ecc4df239658c6e4c07ab7d931e
Rajith <rajithroshan90@gmail.com>
2016-07-26 12:35:28 +0530

    Refactor sso module - New assertion check

--- sso/resources/org.jaggeryjs.modules.sso/src/main/java/org/jaggeryjs/modules/sso/common/util/Util.java
+++ sso/resources/org.jaggeryjs.modules.sso/src/main/java/org/jaggeryjs/modules/sso/common/util/Util.java
@@ -165,10 +165,23 @@ public class Util {
             UnmarshallerFactory unmarshallerFactory = Configuration.getUnmarshallerFactory();
             Unmarshaller unmarshaller = unmarshallerFactory.getUnmarshaller(element);
             response = unmarshaller.unmarshall(element);
-            NodeList list = response.getDOM().getElementsByTagNameNS(SAMLConstants.SAML20P_NS, "Response");
-            if (list.getLength() > 0) {
-                log.error("Invalid schema for the SAML2 reponse");
-                throw new Exception("Error occured while processing saml2 response");
+            if (response instanceof Response) {
+                NodeList list = response.getDOM().getElementsByTagNameNS(SAMLConstants.SAML20P_NS, "Response");
+                if (list.getLength() > 0) {
+                    log.error("Invalid schema for the SAML2 reponse. Multiple response objects found");
+                    throw new Exception("Error occured while processing saml2 response. Multiple response objects found");
+                }
+                NodeList assertionList = response.getDOM().getElementsByTagNameNS(SAMLConstants.SAML20_NS, "Assertion");
+                if (assertionList.getLength() > 1) {
+                    log.error("Invalid schema for the SAML2 response. Multiple assertions detected");
+                    throw new Exception("Error occurred while processing saml2 response. Multiple assertions detected");
+                }
+            } else if (response instanceof Assertion) {
+                NodeList assertionList = response.getDOM().getElementsByTagNameNS(SAMLConstants.SAML20_NS, "Assertion");
+                if (assertionList.getLength() > 0) {
+                    log.error("Invalid schema for the SAML2 response. Multiple assertions detected");
+                    throw new Exception("Error occurred while processing saml2 response. Multiple assertions detected");
+                }
             }
             return response;
         } catch (Exception e) {
